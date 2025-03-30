ca3456ffa54a17920f07c81698c90904fe55e460
theCompanyDream <tbrantleyii@gmail.com>
2025-03-30 15:21:42 -0400

    fix: enviorment test

--- apps/frontend/src/app/user/user.service.ts
+++ apps/frontend/src/app/user/user.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { environment } from '../../environment';
+import { environment } from '../../environments/environment';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import {User} from './user'
