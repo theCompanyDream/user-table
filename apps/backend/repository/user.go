be1167f1c874808c434f13f254b91166a2f475ac
theCompanyDream <tbrantleyii@gmail.com>
2025-03-31 18:42:09 -0400

    this is frustrating

--- apps/backend/repository/user.go
+++ apps/backend/repository/user.go
@@ -60,12 +60,12 @@ func InitDB() error {
 	}
 
 	// Auto migrate with more detailed error handling
-	if _, exists := os.LookupEnv("VERCEL_URL"); !exists {
-		if err := db.AutoMigrate(&model.UserDTO{}); err != nil {
-			// Log the error as a warning and continue
-			fmt.Printf("Warning: Failed to auto migrate: %v", err)
-		}
-	}
+	// if _, exists := os.LookupEnv("VERCEL_URL"); !exists {
+	// 	if err := db.AutoMigrate(&model.UserDTO{}); err != nil {
+	// 		// Log the error as a warning and continue
+	// 		fmt.Printf("Warning: Failed to auto migrate: %v", err)
+	// 	}
+	// }
 
 	fmt.Println("Database connection successful")
 	return nil
